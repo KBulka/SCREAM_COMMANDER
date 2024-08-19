9f9c97a0ae9f0145256591cfec33831a7172a211
KBulka <48280082+KBulka@users.noreply.github.com>
2024-08-19 13:25:30 +0200

    Commit message

---
+++ Source/SCREAM_COMMANDER/SphereLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SphereLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCREAM_COMMANDER_API USphereLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
